b5544331f32af6ef754b697c042db3888f0eea05
evingerbono <bono0823@gmail.com>
2022-10-20 16:07:46 +0200

    orai_munka

--- HarcosVarazslo/src/harcosvarazslo/HarcosVarazslo.java
+++ HarcosVarazslo/src/harcosvarazslo/HarcosVarazslo.java
@@ -10,8 +10,23 @@ public class HarcosVarazslo {
        Random rnd = new Random();
        Scanner sc = new Scanner(System.in); 
        
+       String h = "Harcos";
+       String v = "Varázsló";
+       int helet = 9;
+       int velet = 9;
+       int hhely = rnd.nextInt(3) + 1;
+       int vhely = rnd.nextInt(3) + 1;
+       int dobas = rnd.nextInt(6) + 1;
        
+       System.out.println("A "+h+" életereje: "+helet);
+       System.out.println("A "+h+" helye: "+hhely);
+       System.out.println("");
+       System.out.println("A " + v + " életereje: " + velet);
+       System.out.println("A "+v+" helye: "+vhely);
        
+        if (vhely==hhely) {
+            
+        }
     }
     
 }
