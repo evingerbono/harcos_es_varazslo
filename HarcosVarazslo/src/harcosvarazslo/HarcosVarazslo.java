5155a183b247fe356d351f7af55dcc0ad8922745
evingerbono <bono0823@gmail.com>
2022-10-20 14:13:58 +0200

    Program alap

---
+++ HarcosVarazslo/src/harcosvarazslo/HarcosVarazslo.java
@@ -0,0 +1,17 @@
+
+package harcosvarazslo;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class HarcosVarazslo {
+
+    public static void main(String[] args) {
+       Random rnd = new Random();
+       Scanner sc = new Scanner(System.in); 
+       
+       
+       
+    }
+    
+}
